e008a8b65dfc7935f0f404b245992406a2588d4d
Mbah Babat <79902331+mbahbabat@users.noreply.github.com>
2024-09-10 13:35:29 +0700

    Add files via upload

---
+++ js/livechat.js
@@ -0,0 +1,12 @@
+
+						window.intergramId = "654567263";
+						window.intergramCustomizations = {
+							titleClosed: 'Telegram',
+							titleOpen: 'Admin Telegram',
+							introMessage: '💘🍓  𝓐𝕝Ⓘs𝕒  ♥: Admin Σ sedang online...',
+							autoResponse: '💘🍓  𝓐𝕝Ⓘs𝕒  ♥: Silahkan tunggu ...',
+							autoNoResponse: '💘🍓  𝓐𝕝Ⓘs𝕒  ♥: Silahkan tinggalkan @username telegram kamu jika pesan anda belum ada balasan...' +
+											'',
+							mainColor: "linear-gradient(to left, #6600cc 0%, #ff0000 70%)",
+							alwaysUseFloatingButton: true
+						};

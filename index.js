115ad462be978a651552f3aef27383d1292f0930
Mbah Babat <79902331+mbahbabat@users.noreply.github.com>
2024-12-12 19:45:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -21970,7 +21970,7 @@ async function checkMails(smallParts, totalNeedCheck) {
                 allResult.ver.push(email.email);
 			} else if (email.status === "Disabled") {
                 allResult.disable.push(email.email);			
-            } else if (email.status === "Unregistered") {
+            } else if (email.status === "Error") {
                 allResult.notExist.push(email.email);
 			}					
         });
@@ -22035,7 +22035,7 @@ function report(mails) {
     let good = mails.filter(email => email.status === "live").length;
     let ver = mails.filter(email => email.status === "Verify").length;
     let dis = mails.filter(email => email.status === "Disabled").length;
-    let notExist = mails.filter(email => email.status === "Unregistered").length;
+    let notExist = mails.filter(email => email.status === "Error").length;
     increaseReport("#rp-good", good);
     increaseReport("#rp-ver", ver);
     increaseReport("#rp-disabled", dis);

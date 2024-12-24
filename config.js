9fb94b1f3cc055dc6827bfe7b98eb348d25dfe6e
Mbah Babat <79902331+mbahbabat@users.noreply.github.com>
2024-12-24 07:54:18 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -19,7 +19,8 @@
 			$(".snow_image").toggle();
 			$(".lightning").toggle();
 			$(".lightning2").toggle();
-			$(".announcement").toggle();			
+			$(".announcement").toggle();
+			document.querySelector('#turbo_audio').play();		
 			document.querySelector(".turbo_btn").classList.toggle("turbo_btn_active");
 			document.querySelector("#mail-progress-bar").style.cssText="width:0!important;transition:2s";
 			$("#rp-good").text(" - ");

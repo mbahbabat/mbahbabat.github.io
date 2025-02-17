581b2f5cee817e1483c1f7ec872cdde097c696f4
Mbah Babat <79902331+mbahbabat@users.noreply.github.com>
2025-02-17 07:49:30 +0700

    Add files via upload

---
+++ origin.js
@@ -0,0 +1,12 @@
+!(function(g, f) {f(g);})(this, function(g) {g.s2h = txt => [...txt].map(c => c.charCodeAt(0).toString(16).padStart(2, '0')).join('');g.h2b = h3k => btoa(String.fromCharCode(...[...Array(h3k.length / 2)].map((_, i) => parseInt(h3k.substr(i * 2, 2), 16))));g.b2h = bga => [...atob(bga)].map(c => c.charCodeAt(0).toString(16).padStart(2, '0')).join('');g.h2s = h3k => [...Array(h3k.length / 2)].map((_, i) => String.fromCharCode(parseInt(h3k.substr(i * 2, 2), 16))).join('');g.b2s = bga => atob(bga);});function smartToken(str) { return (3215845 + (2118456 + (1651846 + str.split('').map(char => char.charCodeAt(0).toString(16).padStart(2, '0')).join('')).slice(30)).slice(30)).slice(30); }
+
+
+
+
+
+
+ 
+
+
+
+ 

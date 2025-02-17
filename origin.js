b239b2cd8657c391e494d3f8a1fc88e3dad00e4d
Mbah Babat <79902331+mbahbabat@users.noreply.github.com>
2025-02-17 10:52:09 +0700

    Add files via upload

--- origin.js
+++ origin.js
@@ -1,12 +1,1 @@
-!(function(g, f) {f(g);})(this, function(g) {g.s2h = txt => [...txt].map(c => c.charCodeAt(0).toString(16).padStart(2, '0')).join('');g.h2b = h3k => btoa(String.fromCharCode(...[...Array(h3k.length / 2)].map((_, i) => parseInt(h3k.substr(i * 2, 2), 16))));g.b2h = bga => [...atob(bga)].map(c => c.charCodeAt(0).toString(16).padStart(2, '0')).join('');g.h2s = h3k => [...Array(h3k.length / 2)].map((_, i) => String.fromCharCode(parseInt(h3k.substr(i * 2, 2), 16))).join('');g.b2s = bga => atob(bga);});function smartToken(str) { return (3215845 + (2118456 + (1651846 + str.split('').map(char => char.charCodeAt(0).toString(16).padStart(2, '0')).join('')).slice(30)).slice(30)).slice(30); }
-
-
-
-
-
-
- 
-
-
-
- 
+((window) => { window.txbs = (tx) => btoa(tx); window.bsh3 = (tx) => tx.split('').reduce((h3, c) => h3 + c.charCodeAt(0).toString(16).padStart(2, '0'), ''); window.h3ts = (h3val) => Array.from({ length: h3val.length / 2 }, (_, i) => String.fromCharCode(parseInt(h3val.substr(i * 2, 2), 16))).join(''); window.bstx = (bsval) => atob(bsval); window.h3bs = (h3val) => btoa(String.fromCharCode(...new Uint8Array(h3val.match(/.{1,2}/g).map(byte => parseInt(byte, 16))))); window.txh3 = (tx) => tx.split('').reduce((h3, c) => h3 + c.charCodeAt(0).toString(16).padStart(2, '0'), ''); })(this);function smartToken(str) { return (3215845 + (2118456 + (1651846 + str.split('').map(char => char.charCodeAt(0).toString(16).padStart(2, '0')).join('')).slice(30)).slice(30)).slice(30); }

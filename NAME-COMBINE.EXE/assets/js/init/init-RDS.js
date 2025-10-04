75036abe2ccef43307cdb1432510ac52f91b2427
Mbah Babat <79902331+mbahbabat@users.noreply.github.com>
2025-10-04 21:16:51 +0700

    Add files via upload

--- NAME-COMBINE.EXE/assets/js/init/init-RDS.js
+++ NAME-COMBINE.EXE/assets/js/init/init-RDS.js
@@ -1,1 +1,1 @@
-import{RDS}from"../lib/RDS.js";RDS.init({debug:!1,breakpoint:1024,desktop:{baseWidth:1980,baseHeight:1080,minScale:.5,maxScale:1.2},mobile:{baseWidth:767,baseHeight:1080,minScale:.3,maxScale:1.1},afterUpdate:e=>{}});
+import{RDS}from"../lib/RDS.js";RDS.init({debug:!1,breakpoint:1024,desktop:{baseWidth:1980,baseHeight:1080,minScale:.5,maxScale:1.2},mobile:{baseWidth:600,baseHeight:1080,minScale:.3,maxScale:1.1},afterUpdate:e=>{}});
